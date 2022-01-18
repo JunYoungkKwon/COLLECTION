a64f5622ee5bdb616f6eced6b94d0b3b60276d2b
SUMIN KIM <smo1621@naver.com>
2022-01-18 17:28:02 +0900

    Change Top Button to Radio Button

--- app/src/main/java/com/eight/collection/ui/writing/TopRVAdapter.kt
+++ app/src/main/java/com/eight/collection/ui/writing/TopRVAdapter.kt
@@ -7,6 +7,22 @@ import com.eight.collection.databinding.ItemWritefirstPhotoBinding
 import com.eight.collection.databinding.ItemWritefirstTopBinding
 
 class TopRVAdapter(private val topList: ArrayList<Top>) : RecyclerView.Adapter<TopRVAdapter.ViewHolder>(){
+
+    private var selectCheck : ArrayList<Int> = arrayListOf()
+
+
+    init {
+        for(i in topList){
+            if(i.title == "+"){
+                selectCheck.add(1)
+            }
+            else{
+                selectCheck.add(0)
+            }
+        }
+    }
+
+
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): TopRVAdapter.ViewHolder {
         val binding : ItemWritefirstTopBinding = ItemWritefirstTopBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
 
@@ -22,7 +38,21 @@ class TopRVAdapter(private val topList: ArrayList<Top>) : RecyclerView.Adapter<T
 
     inner class ViewHolder(val binding: ItemWritefirstTopBinding) : RecyclerView.ViewHolder(binding.root){
         fun bind(top:Top){
-            binding.writefirstColorTopTextButton.text = top.title
+            binding.writefirstColorTopTextButton.apply {
+                text = top.title
+                isChecked = selectCheck[bindingAdapterPosition] == 1
+                setOnClickListener{
+                    for (k in selectCheck.indices) {
+                        if (k == bindingAdapterPosition) {
+                            selectCheck[k] = 1
+                        }
+                        else {
+                            selectCheck[k] = 0
+                        }
+                    }
+                    notifyDataSetChanged()
+                }
+            }
         }
     }
 }
