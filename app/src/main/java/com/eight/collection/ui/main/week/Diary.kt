d284d57e6895d633867ca85ca43da1aa6ea94f57
gypig <gypig12@gachon.ac.kr>
2022-01-18 11:20:20 +0900

    nested recylcer view

---
+++ app/src/main/java/com/eight/collection/ui/main/week/Diary.kt
@@ -0,0 +1,8 @@
+package com.eight.collection.ui.main.week
+
+data class Diary(
+//    var ratingPoint : Int? = null,
+//    var color : String? = "",
+    var coverImg : Int? = null,
+    var imgCount: Int? = null
+)
---
+++ app/src/main/java/com/eight/collection/ui/main/week/DiaryRVAdapter.kt
@@ -0,0 +1,39 @@
+package com.eight.collection.ui.main.week
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.eight.collection.databinding.ItemWeekDiaryBinding
+
+
+class DiaryRVAdapter(private  val Diarylist: ArrayList<Diary>) : RecyclerView.Adapter<DiaryRVAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): DiaryRVAdapter.ViewHolder {
+        val binding: ItemWeekDiaryBinding = ItemWeekDiaryBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        return  ViewHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(holder: DiaryRVAdapter.ViewHolder, position: Int) {
+        holder.bind(Diarylist[position])
+    }
+
+
+    override fun getItemCount(): Int = Diarylist.size
+
+
+    inner class ViewHolder(val binding: ItemWeekDiaryBinding): RecyclerView.ViewHolder(binding.root){
+
+        fun bind(diary: Diary){
+            binding.itemDiaryImgCountTv.text= diary.imgCount.toString()
+            binding.itemDiaryImgIv.setImageResource(diary.coverImg!!)
+
+            binding.weekDiaryMoodRecyclerView.adapter = MoodRVAdapter(diary.Dia)
+        }
+    }
+
+
+}
+
+
