91009c100beb1009e776b2f4a602025c1e9d6428
gypig <gypig12@gachon.ac.kr>
2022-01-19 18:46:50 +0900

    fragment-fragment move

---
+++ app/src/main/java/com/eight/collection/ui/main/lookpoint/LookPointFragment.kt
@@ -0,0 +1,13 @@
+package com.eight.collection.ui.main.lookpoint
+
+import android.view.View
+import com.eight.collection.databinding.FragmentLookPointBinding
+import com.eight.collection.databinding.FragmentMonthBinding
+import com.eight.collection.databinding.FragmentMatchBinding
+import com.eight.collection.ui.BaseFragment
+
+ class LookPointFragment(): BaseFragment<FragmentLookPointBinding>(FragmentLookPointBinding::inflate) {
+    override fun initAfterBinding() {
+
+    }
+}
---
+++ app/src/main/java/com/eight/collection/ui/writing/FinishActivity.kt
@@ -0,0 +1,13 @@
+package com.eight.collection.ui.writing
+
+import com.eight.collection.databinding.ActivityFinishBinding
+import com.eight.collection.ui.BaseActivity
+
+class FinishActivity :BaseActivity<ActivityFinishBinding>(ActivityFinishBinding::inflate) {
+
+    override fun initAfterBinding() {
+
+
+    }
+
+}
