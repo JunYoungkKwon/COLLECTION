25de4c582bd556fa1a05b9584c04407457680e86
gypig <gypig12@gachon.ac.kr>
2022-02-09 18:49:15 +0900

    setting page complete / design edit

--- app/src/main/java/com/eight/collection/ui/main/month/Month.kt
+++ app/src/main/java/com/eight/collection/ui/main/month/Month.kt
@@ -1,6 +1,6 @@
 package com.eight.collection.ui.main.month
 
-import com.eight.collection.ui.main.lookpoint.Photo
+import com.eight.collection.ui.main.lookpoint.Photo//
 
 data class Month(
     var point : Int? = null,
