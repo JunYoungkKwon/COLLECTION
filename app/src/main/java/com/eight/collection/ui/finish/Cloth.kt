e05641c15542ef3ba28d73c8740b84137ee7ff06
gypig <gypig12@gachon.ac.kr>
2022-02-26 23:19:11 +0900

    Finish API finish

--- app/src/main/java/com/eight/collection/ui/finish/Cloth.kt
+++ app/src/main/java/com/eight/collection/ui/finish/Cloth.kt
@@ -2,7 +2,7 @@ package com.eight.collection.ui.finish
 
 import com.google.gson.annotations.SerializedName
 
-data class Bottom(
+data class Cloth(
     @SerializedName("smallClass") var cloth : String? = "",
-    @SerializedName("color") var color : String? = ""
+    @SerializedName("color") var color : String? = "",
 )
