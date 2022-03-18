ada36bda7266355c6f149262167283a0e9567166
SUMIN KIM <smo1621@naver.com>
2022-03-18 23:33:00 +0900

    Matching Weather Design Commit

--- app/src/main/java/com/eight/collection/ui/main/match/weather/WeatherActivity.kt
+++ app/src/main/java/com/eight/collection/ui/main/match/weather/WeatherActivity.kt
@@ -17,7 +17,7 @@ import com.eight.collection.utils.getIntroduceIs
 class WeatherActivity: BaseActivity<ActivityMatchWeatherBinding>(ActivityMatchWeatherBinding::inflate) {
 
     override fun initAfterBinding() {
-        // EditText (검색창) 눌렀을시 이벤트
+        // 검색창 눌렀을시 이벤트
         binding.matchWeatherSearchViewTv.setOnClickListener{
             binding.matchWeatherDefaultTv.visibility = View.GONE
             binding.matchWeatherVeryColdBt.visibility = View.GONE
@@ -36,8 +36,11 @@ class WeatherActivity: BaseActivity<ActivityMatchWeatherBinding>(ActivityMatchWe
             binding.matchWeatherSearchButtonRecyclerview.visibility = View.VISIBLE
             binding.matchWeatherSearchBt.visibility = View.VISIBLE
             binding.matchWeatherSearchEt.visibility = View.VISIBLE
+            binding.matchWeatherSearchClothIv.visibility = View.VISIBLE
+            binding.matchWeatherSearchTv.visibility = View.VISIBLE
         }
 
+        // 돌아가기창 눌렀을시 이벤트
         binding.matchWeatherSearchBackIc.setOnClickListener{
             binding.matchWeatherDefaultTv.visibility = View.VISIBLE
             binding.matchWeatherVeryColdBt.visibility = View.VISIBLE
@@ -54,9 +57,63 @@ class WeatherActivity: BaseActivity<ActivityMatchWeatherBinding>(ActivityMatchWe
             binding.matchWeatherSearchBackIc.visibility = View.GONE
             binding.matchWeatherSearchDeleteIc.visibility = View.GONE
             binding.matchWeatherSearchButtonRecyclerview.visibility = View.GONE
-            binding.matchWeatherSearchBt.visibility = View.GONE
             binding.matchWeatherSearchEt.setText("")
             binding.matchWeatherSearchEt.visibility = View.GONE
+
+            binding.matchWeatherSearchBt.visibility = View.GONE
+            binding.matchWeatherSearchClothIv.visibility = View.GONE
+            binding.matchWeatherSearchTv.visibility = View.GONE
+
+            binding.matchWeatherSearchResultFirstTv.visibility = View.GONE
+            binding.matchWeatherSearchResultSecondTv.visibility = View.GONE
+            binding.matchWeatherSearchResultThirdTv.visibility = View.GONE
+            binding.matchWeatherSearchDayBt.visibility = View.GONE
+            binding.matchWeatherSearchLastBt.visibility = View.GONE
+            binding.weekDiaryRecyclerView.visibility = View.GONE
         }
+
+        // 검색창 x버튼 눌렀을시 이벤트
+        binding.matchWeatherSearchDeleteIc.setOnClickListener{
+            binding.matchWeatherSearchEt.setText("")
+
+            binding.matchWeatherSearchBt.visibility = View.VISIBLE
+            binding.matchWeatherSearchClothIv.visibility = View.VISIBLE
+            binding.matchWeatherSearchTv.visibility = View.VISIBLE
+
+            binding.matchWeatherSearchResultFirstTv.visibility = View.GONE
+            binding.matchWeatherSearchResultSecondTv.visibility = View.GONE
+            binding.matchWeatherSearchResultThirdTv.visibility = View.GONE
+            binding.matchWeatherSearchDayBt.visibility = View.GONE
+            binding.matchWeatherSearchLastBt.visibility = View.GONE
+            binding.weekDiaryRecyclerView.visibility = View.GONE
+        }
+
+        // 검색 버튼 눌렀을시 이벤트
+        binding.matchWeatherSearchBt.setOnClickListener{
+            binding.matchWeatherSearchBt.visibility = View.GONE
+            binding.matchWeatherSearchClothIv.visibility = View.GONE
+            binding.matchWeatherSearchTv.visibility = View.GONE
+
+            binding.matchWeatherSearchResultFirstTv.visibility = View.VISIBLE
+            binding.matchWeatherSearchResultSecondTv.visibility = View.VISIBLE
+            binding.matchWeatherSearchResultThirdTv.visibility = View.VISIBLE
+            binding.matchWeatherSearchDayBt.visibility = View.VISIBLE
+            binding.matchWeatherSearchLastBt.visibility = View.VISIBLE
+            binding.weekDiaryRecyclerView.visibility = View.VISIBLE
+        }
+
+
+        //최신순 버튼 눌렀을시 이벤트
+        binding.matchWeatherSearchLastBt.setOnClickListener{
+            binding.matchWeatherSearchLastBt.visibility = View.INVISIBLE
+            binding.matchWeatherSearchOldBt.visibility = View.VISIBLE
+        }
+
+        binding.matchWeatherSearchOldBt.setOnClickListener{
+            binding.matchWeatherSearchLastBt.visibility = View.VISIBLE
+            binding.matchWeatherSearchOldBt.visibility = View.INVISIBLE
+        }
+
+
     }
 }
